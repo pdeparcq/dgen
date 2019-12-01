18d1b2cc477ced895554757adf8a2ad325c54677
Pieter Deparcq <pdeparcq@gmail.com>
2019-12-01 12:56:00 +0100

    Add project files.

---
+++ DGen.Test/SandBox.cs
@@ -0,0 +1,31 @@
+﻿using DGen.Test.StarUml;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace DGen.Test
+{
+    [TestFixture]
+    public class SandBox
+    {
+        [Test]
+        public void CanLoadStarUmlModel()
+        {
+            var json = File.ReadAllText(@"C:\Users\pdepa\OneDrive\Documenten\MyLunch.mdj");
+            json = json.Replace("_id", "$id");
+
+            var settings = new JsonSerializerSettings()
+            {
+                MetadataPropertyHandling = MetadataPropertyHandling.ReadAhead,
+                PreserveReferencesHandling = PreserveReferencesHandling.Objects,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                Formatting = Formatting.Indented
+            };
+
+            var model = JsonConvert.DeserializeObject<Element>(json, settings);
+
+            Console.WriteLine(JsonConvert.SerializeObject(model, settings));
+        }
+    }
+}
---
+++ DGen.Test/StarUml/StarUmlModel.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using System.Collections.Generic;
+
+namespace DGen.Test.StarUml
+{
+    public enum ElementType
+    {
+        Project,
+        UMLModel,
+        UMLPackage,
+        UMLClass,
+        UMLAssociation,
+        UMLAssociationEnd,
+        UMLDependency,
+        UMLEnumeration,
+        UMLEnumerationLiteral,
+        UMLClassDiagram,
+        UMLAttribute
+    }
+
+    public class Element
+    {
+        [JsonProperty(PropertyName = "_type")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public ElementType Type { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "_parent", IsReference = true)]
+        public Element Parent { get; set; }
+
+        [JsonProperty(PropertyName = "ownedElements")]
+        public List<Element> OwnedElements { get; set; }
+
+        [JsonProperty(PropertyName = "stereotype")]
+        public string Stereotype { get; set; }
+
+        [JsonProperty(PropertyName = "attributes")]
+        public List<Element> Attributes { get; set; }
+
+        [JsonProperty(PropertyName = "literals")]
+        public List<Element> Literals { get; set; }
+
+        [JsonProperty(PropertyName = "reference", IsReference = true)]
+        public Element Reference { get; set; }
+
+        [JsonProperty(PropertyName = "end1")]
+        public Element AssociationEndFrom { get; set; }
+
+        [JsonProperty(PropertyName = "end2")]
+        public Element AssociationEndTo { get; set; }
+
+        [JsonProperty(PropertyName = "multiplicity")]
+        public string Multiplicity { get; set; }
+    }
+}
