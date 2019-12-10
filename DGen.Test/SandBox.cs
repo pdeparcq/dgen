ec23797b05599e3c0c6b9c51ad93b4a37b03a57a
Pieter Deparcq <pdeparcq@gmail.com>
2019-12-10 20:11:49 +0100

    Including test model

--- DGen.Test/SandBox.cs
+++ DGen.Test/SandBox.cs
@@ -10,31 +10,38 @@ using DGen.Test.Generation.Application;
 using DGen.Test.Generation.Domain;
 using DGen.Test.Generation.Infrastructure;
 using DGen.Test.Generation.Presentation;
+using System.Reflection;
+using System.IO;
 
 namespace DGen.Test
 {
     [TestFixture]
     public class SandBox
     {
+
         [Test]
         public void CanReadStarUmlModel()
         {
-            var model = new StarUmlReader().Read(@"C:\dev\poc\HelloCustomer.mdj");
+            var model = new StarUmlReader().Read(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "MyLunch.mdj"));
             Console.WriteLine(JsonConvert.SerializeObject(model, Formatting.Indented));
         }
 
         [Test]
         public void CanGenerateMetaModel()
         {
-            var model = new StarUmlReader().Read(@"C:\dev\poc\HelloCustomer.mdj");
+            var model = new StarUmlReader().Read(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "MyLunch.mdj"));
             var metaModel = new MetaModelGenerator().Generate(model);
-            Console.WriteLine(JsonConvert.SerializeObject(metaModel, Formatting.Indented));
+            Console.WriteLine(JsonConvert.SerializeObject(metaModel, Formatting.Indented,
+                new JsonSerializerSettings
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                }));
         }
 
         [Test]
         public async Task CanGenerateCodeFromMetaModel()
         {
-            var model = new StarUmlReader().Read(@"C:\dev\poc\MyLunch.mdj");
+            var model = new StarUmlReader().Read(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "MyLunch.mdj"));
             var metaModel = new MetaModelGenerator().Generate(model);
 
             var generators = new List<ICodeGenerator>
@@ -45,7 +52,7 @@ namespace DGen.Test
                 new PresentationCodeGenerator()
             };
 
-            await new CodeGenerator(generators).Generate(metaModel, @"C:\dev\poc\generated");
+            await new CodeGenerator(generators).Generate(metaModel, Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "generated"));
         }
     }
 }
